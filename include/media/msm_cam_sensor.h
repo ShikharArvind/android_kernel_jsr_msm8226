1182f247e81ec84abb75c19da87c01ad0dd3e527
remittor <remittor@gmail.com>
2015-03-19 19:26:28 +0300

    Revert "msm: camera2: add api for set actuator for manual af"
    This reverts commit a5d15da46bc1fa3467310e2c5aa998c365a78c00.
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
    	include/media/msm_cam_sensor.h

--- include/media/msm_cam_sensor.h
+++ include/media/msm_cam_sensor.h
@@ -51,7 +51,6 @@
 #define MAX_EEPROM_NAME 32
 
 #define MAX_AF_ITERATIONS 3
-#define MAX_NUMBER_OF_STEPS 47
 #define MAX_POWER_CONFIG 12
 
 typedef enum sensor_stats_type {
@@ -491,7 +490,6 @@ enum msm_actuator_cfg_type_t {
 	CFG_SET_ACTUATOR_INFO,
 	CFG_SET_DEFAULT_FOCUS,
 	CFG_MOVE_FOCUS,
-	CFG_SET_POSITION,
 	CFG_ACTUATOR_POWERDOWN,
 	CFG_ACTUATOR_POWERUP,
 };
@@ -600,13 +598,6 @@ enum af_camera_name {
 	ACTUATOR_WEB_CAM_2,
 };
 
-
-struct msm_actuator_set_position_t {
-	uint16_t number_of_steps;
-	uint16_t pos[MAX_NUMBER_OF_STEPS];
-	uint16_t delay[MAX_NUMBER_OF_STEPS];
-};
-
 struct msm_actuator_cfg_data {
 	int cfgtype;
 	uint8_t is_af_supported;
@@ -614,7 +605,6 @@ struct msm_actuator_cfg_data {
 		struct msm_actuator_move_params_t move;
 		struct msm_actuator_set_info_t set_info;
 		struct msm_actuator_get_info_t get_info;
-		struct msm_actuator_set_position_t setpos;
 		enum af_camera_name cam_name;
 	} cfg;
 };
